c8d8462b4eb1b9511d4b598b2fe41120934d8760
dineshreva <dinesh.redmipro@gmail.com>
2023-03-12 16:33:34 +0530

    added hello print statement

--- repoproject/src/main/java/repod/repoproject/JavaSampleClass.java
+++ repoproject/src/main/java/repod/repoproject/JavaSampleClass.java
@@ -3,6 +3,7 @@ package repod.repoproject;
 public class JavaSampleClass {
 	public static void main(String args[]) {
 		System.out.println("Hi lets go for lunch please go ");
+		System.out.println("Hello");
 		
 	}
 
