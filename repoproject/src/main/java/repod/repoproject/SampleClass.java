dc571ee3f31ccbfc6e060be026795dbd3b9c84b0
dineshreva <dinesh.redmipro@gmail.com>
2023-03-12 15:10:51 +0530

    added

--- repoproject/src/main/java/repod/repoproject/SampleClass.java
+++ repoproject/src/main/java/repod/repoproject/SampleClass.java
@@ -3,6 +3,7 @@ package repod.repoproject;
 public class SampleClass {
 public static void main(String args[]) {
 	System.out.println("Hello display some messages on the screen");
-	System.out.println("Lets talk about the content which are used to follow ");
+	System.out.println("Lets talk about the content which are used to follow the content ");
+	
 }
 }
