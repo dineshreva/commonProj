45803e8846171ae394f585ce4467abf6f052ec9b
dineshreva <dinesh.redmipro@gmail.com>
2023-03-12 15:09:17 +0530

    new class get created in the project

---
+++ repoproject/src/main/java/repod/repoproject/SampleClass.java
@@ -0,0 +1,8 @@
+package repod.repoproject;
+
+public class SampleClass {
+public static void main(String args[]) {
+	System.out.println("Hello display some messages on the screen");
+	System.out.println("Lets talk about the content which are used to follow ");
+}
+}
