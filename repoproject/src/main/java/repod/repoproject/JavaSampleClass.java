5204e761f394b8e70c638a9083c6aa9daa65f420
dineshreva <dinesh.redmipro@gmail.com>
2023-03-12 16:48:21 +0530

    added new functionality to the project and created new class

--- repoproject/src/main/java/repod/repoproject/JavaSampleClass.java
+++ repoproject/src/main/java/repod/repoproject/JavaSampleClass.java
@@ -4,6 +4,9 @@ public class JavaSampleClass {
 	public static void main(String args[]) {
 		System.out.println("Hi lets go for lunch please go ");
 		System.out.println("Hello");
+		MobileNewClass mb = new MobileNewClass();
+		System.out.println(mb.add(10, 20));
+		
 		
 	}
 
---
+++ repoproject/src/main/java/repod/repoproject/MobileNewClass.java
@@ -0,0 +1,10 @@
+package repod.repoproject;
+
+public class MobileNewClass {
+	
+	public int add(int a, int b) {
+		int c = a+b;
+		return c;
+	}
+
+}
